c4cbd94820e3f10c64b8bafd8fb01e41882eefed
Cho0 <choojason7@gmail.com>
2021-07-07 20:46:31 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+alert('Hello World');
+
+let myName = 'Jason';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
