b561142b92c6eeae14ea96c835e37f662d05f4c1
Cho0 <choojason7@gmail.com>
2021-07-07 20:53:41 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,4 @@
 alert('Hello World');
 
-let myName = 'Jason';
-document.write(myName);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
+let favoriteFood = 'Soba Noodles';
+document.write(favoriteFood);
