bfde697cdb1d711f21f6b0176ef5d5c09f586625
Cho0 <choojason7@gmail.com>
2021-07-08 21:39:21 -0700

    Added pokemon array
    List of pokemon and their qualities

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello World');
-
-let favoriteFood = 'Soba Noodles';
-document.write(favoriteFood);
+let pokemonList = [
+	{name: "Bulbasaur", type: ["grass", "poison"], height: "0.7m"}, 
+	{name: "Ivysaur", type: ["grass", "poison"], height: "1m"},
+	{name: "Venusaur", type: ["grass", "poison"], height: "2m"},
+	{name: "Charmander", type: "fire", height: "0.6m"},
+	{name: "Charmeleon", type: "fire", height: "1.1m"},
+	{name: "Charizard", type: ["fire", "flying"], height: "1.7m"}
+	]
