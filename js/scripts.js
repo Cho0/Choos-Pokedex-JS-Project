585e8438ed77bea0c76a94382f3c4d340f9b953e
Cho0 <choojason7@gmail.com>
2021-07-09 08:57:39 -0700

    added brackets for single arrays

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ let pokemonList = [
 	{name: "Bulbasaur", type: ["grass", "poison"], height: "0.7m"}, 
 	{name: "Ivysaur", type: ["grass", "poison"], height: "1m"},
 	{name: "Venusaur", type: ["grass", "poison"], height: "2m"},
-	{name: "Charmander", type: "fire", height: "0.6m"},
-	{name: "Charmeleon", type: "fire", height: "1.1m"},
+	{name: "Charmander", type: ["fire"], height: "0.6m"},
+	{name: "Charmeleon", type: ["fire"], height: "1.1m"},
 	{name: "Charizard", type: ["fire", "flying"], height: "1.7m"}
 	]
